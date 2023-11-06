4df1a8d1620b8e2148a595a675d6a3f2ff74b81a
Lilian Almeida <lili.l2002@hotmail.com>
2023-11-06 14:25:39 -0300

    react rouber e tailwing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 import './App.css'
-import Home from './pages/home/Home'
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
 
 function App() {
     return (
-        <Home />
-    )
-}
-
-export default App
+      <>
+        <BrowserRouter>
+          <Navbar />
+            <div className='min-h-[80vh]'>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+              </Routes>
+            </div>  
+          <Footer />
+        </BrowserRouter>
+      </>
+    );
+  }
+  
+  export default App;
