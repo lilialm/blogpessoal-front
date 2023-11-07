58768af2303c4c15fabaf55a24fca3ddca419f66
Lilian Almeida <lili.l2002@hotmail.com>
2023-11-07 17:08:05 -0300

    Cadastro, Login e deslogar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,9 @@
 
+
 function Home() {
+
+    
+
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
@@ -13,9 +17,7 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
-                                >
+                            <div className='rounded text-white border-white border-solid border-2 py-2 px-4'>
                                 Nova Postagem
                             </div>
                         </div>
